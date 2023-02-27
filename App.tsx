550d732fa9e91e367e54df2ccef1437a9cbb55c8
Hossein Mehrmohammadi <hossein.m.1459@gmail.com>
2023-02-27 14:32:20 +0330

    initialize expo project

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import BookList from './app/components/bookList';
+import { Provider } from 'react-redux';
+import store from './app/redux/store';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <View style={styles.container}>
+        <BookList />
+        {/* <StatusBar style="auto" /> */}
+      </View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '300px'
+  },
+});
